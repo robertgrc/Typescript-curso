699b442e78e19a8be85e4570f1cdecbe1698b426
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-11 17:15:03 -0400

    objetos personalizados typescipt

---
+++ src/objetos/tipoPersonalizado.ts
@@ -0,0 +1,24 @@
+
+type Hero ={
+    name:string;
+    age?:number;
+    powers:string[];
+    getNombre?:()=>string;
+}
+
+
+export let flashName: Hero ={
+    name:"Barry Allen",
+    age: 24,
+    powers:['Super velocidad','viajar en el tiempos']
+}
+export let supermanName: Hero ={
+    name:"Barry Allen",
+    age: 24,
+    powers:['Super velocidad','viajar en el tiempos'],
+    getNombre(){
+        return this.name
+    }
+}
+
+console.log({supermanName})
