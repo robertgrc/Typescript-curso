36e8e7ec24b2b7a1140c2da3d98f10640a54d30c
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-13 10:33:39 -0400

    Forma corta de asignar propiedades typescript

--- src/bases/clases/ClasesBasico.ts
+++ src/bases/clases/ClasesBasico.ts
@@ -1,19 +1,17 @@
 
  export class Avenger {
-    private name: string;
-    private team: string;
-    public realName?: string;
+  
     static avgAge: number=35;
 
-    constructor(name:string, team:string, realName?:string){
-        this.name=name;
-        this.team=team;
-        this.realName = realName;
-    }
+    constructor(
+        private name:string, 
+        private team:string, 
+        public realName?:string
+        ){}
 
 }
 
-export const antman: Avenger = new Avenger("Antman","Capitan");
+export const antman: Avenger = new Avenger("Antman","Capitan","Scott Lang");
 console.log(antman)
 console.log(Avenger.avgAge)
 
