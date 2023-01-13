c96cc5aa7403e809b73c0be00bf30c5b89cb51af
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-13 15:30:27 -0400

    Interfaces para las funciones

---
+++ src/interfacesTypescript/funcionesInterfases.ts
@@ -0,0 +1,10 @@
+
+interface addTwoNumbers {
+    (a:number, b:number): number;
+}
+
+export let addNumbersFunction: addTwoNumbers;
+
+addNumbersFunction = (a:number, b:number)=>{
+    return 10;
+}
