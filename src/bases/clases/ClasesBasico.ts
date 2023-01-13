ec5bb4fd4db599d6fe870803b73ea77359d6a61a
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-13 10:28:05 -0400

    Definicion de una clase basica en Typescript

--- src/bases/clases/ClasesBasico.ts
+++ src/bases/clases/ClasesBasico.ts
@@ -1,5 +1,19 @@
 
-// class Avenger {
-//     private name: string;
-//     public team: string;
-// }
+ export class Avenger {
+    private name: string;
+    private team: string;
+    public realName?: string;
+    static avgAge: number=35;
+
+    constructor(name:string, team:string, realName?:string){
+        this.name=name;
+        this.team=team;
+        this.realName = realName;
+    }
+
+}
+
+export const antman: Avenger = new Avenger("Antman","Capitan");
+console.log(antman)
+console.log(Avenger.avgAge)
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import { setupCounter } from './counter'
 import { charmander } from './basesNest/04-injection'
+import { antman } from './bases/clases/ClasesBasico'
 //import { Ngolo } from './basesNest/03-classes'
 
 
@@ -8,6 +9,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
   
     <h1>hello ${charmander.name}</h1>
+    <h1>hello ${console.log(antman)}</h1>
   </div>
 `
 
