0c42d15b850887522fd0edfbd5ac69096107d635
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-01-13 14:01:38 -0400

    interfaces-estructuras complejas

---
+++ src/interfacesTypescript/complejas.ts
@@ -0,0 +1,38 @@
+
+
+
+interface Client {
+    name: string;
+    age?: number;
+    address: Address
+}
+
+interface Address{
+    id: number;
+    zip: string;
+    city: string;
+}
+
+export const client:Client = {
+    name: "Roco",
+    age: 40,
+    address:{
+        id:125,
+        zip:"KY2 SUD",
+        city:"Ottawa"
+    }
+}
+
+export const client2: Client ={
+    name: "Melissa",
+    age: 30,
+    address:{
+        id:77,
+        zip:"cowabunga",
+        city:"republica"
+
+    }
+}
+
+console.log({client})
+console.log({client2})
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
 import { setupCounter } from './counter'
 import { flashName } from './interfacesTypescript/basicas'
+import { client } from './interfacesTypescript/complejas'
 
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>${flashName.name}</h1>
+    <h1>${client.name}</h1>
   </div>
 `
 
